13865290a923031b01fbd5d5de24a1c63f69c95d
Rupophobe <92577037+Rupophobe@users.noreply.github.com>
2022-01-14 15:36:08 +0100

    Query2

--- Query2/__init__.py
+++ Query2/__init__.py
@@ -1,5 +1,9 @@
 import logging
-
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
+import os
+import pyodbc as pyodbc
 import azure.functions as func
 
 
@@ -14,6 +18,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
+
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+
+    errorMessage = ""
+    dataString = ""
+    try:
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT TOP(10) primaryName FROM tNames WHERE birthYear = 1960")
+
+            rows = cursor.fetchall()
+            for row in rows:
+                dataString += f"SQL: tconst={row[0]}, primaryName={row[1]}\n"
+
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
