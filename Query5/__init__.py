811e42d610cff5f3002bf6822975fa1eab2e1c2e
Rupophobe <92577037+Rupophobe@users.noreply.github.com>
2022-01-23 18:37:44 +0100

    Update __init__.py

--- Query5/__init__.py
+++ Query5/__init__.py
@@ -1,24 +1,62 @@
 import logging
-
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
+import os
+import pyodbc as pyodbc
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    critere = req.params.get('critere')
+    if not critere:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            critere = req_body.get('critere')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+        
+    errorMessage = ""
+    dataString = ""
+    try:
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            if critere == 'genre' or not critere:
+                cursor.execute("select t2.genre, avg(t1.runtimeMinutes) from [dbo].[tTitles] as t1 join [dbo].[tGenres] as t2 on t1.tconst = t2.tconst group by t2.genre")
+                rows = cursor.fetchall()
+                for row in rows:
+                    dataString += f"genre={row[0]}, temps_moyen={row[1]}\n"
+
+            elif critere == 'acteur':
+                cursor.execute("select t3.primaryName, avg(t1.runtimeMinutes) from [dbo].[tTitles] as t1 join [dbo].[tPrincipals] as t2 on t1.tconst = t2.tconst join [dbo].[tNames] as t3 on t2.nconst = t3.nconst where t2.category = 'acted in' group by t3.primaryName")
+                rows = cursor.fetchall()
+                for row in rows:
+                    dataString += f"nom={row[0]}, temps_moyen={row[1]}\n"
+
+            elif critere == 'directeur':
+                cursor.execute("select t3.primaryName, avg(t1.runtimeMinutes) from [dbo].[tTitles] as t1 join [dbo].[tPrincipals] as t2 on t1.tconst = t2.tconst join [dbo].[tNames] as t3 on t2.nconst = t3.nconst where t2.category = 'directed' group by t3.primaryName")
+                rows = cursor.fetchall()
+                for row in rows:
+                    dataString += f"nom={row[0]}, temps_moyen={row[1]}\n"
+
+    except:
+        errorMessage = "Erreur de connexion a la base SQL"
+
+    return func.HttpResponse(dataString)
